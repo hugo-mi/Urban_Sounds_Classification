65b0f89d33b6803add0f04ba47f1f91553b8e077
Thomas R <129596360+TR-tryingnewstuff@users.noreply.github.com>
2024-06-09 17:10:42 +0200

    Add files via upload

--- predict_model.py
+++ predict_model.py
@@ -1,12 +1,16 @@
 import torch
 from device import device
-
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, classification_report
 # ----------------------------
 # Inference
 # ----------------------------
+
 def inference (model, val_dl):
   correct_prediction = 0
   total_prediction = 0
+  all_preds = []
+  all_labels = []
 
   # Disable gradient updates
   with torch.no_grad():
@@ -23,9 +27,14 @@ def inference (model, val_dl):
 
       # Get the predicted class with the highest score
       _, prediction = torch.max(outputs,1)
+      all_preds.extend(prediction)
+      all_labels.extend(labels)
       # Count of predictions that matched the target label
       correct_prediction += (prediction == labels).sum().item()
       total_prediction += prediction.shape[0]
-    
+  
   acc = correct_prediction/total_prediction
   print(f'Accuracy: {acc:.2f}, Total items: {total_prediction}')
+
+  print(classification_report(all_labels, all_preds))
+  sns.heatmap(confusion_matrix(all_labels, all_preds))
